203b955560df96da322f7866abdc649d97e7c744
jasehackman <jasehackman@gmail.com>
2019-01-25 15:26:38 -0600

    added employee view and html

--- Bangazon/views.py
+++ Bangazon/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 # Create your views here.
+
+def employees(request):
+  employee_list = Employee.objects.all()
+  context = {'employee_list': employee_list}
+  return render(request, 'Bangazon/employees.html', context)
